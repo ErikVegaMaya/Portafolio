5777f08e992602e25c75bfda49fbf121abfe44c7
Erik Vega <erikvm95@gmail.com>
2023-01-18 19:05:39 -0600

    delete and edit icons added

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,3 +3,11 @@ import React from "react";
 export const CloseIcon = () => {
   return <span className=" material-icons">close</span>;
 };
+
+export const DeleteIcon = () => {
+  return <span className=" material-icons">delete</span>;
+};
+
+export const EditIcon = () => {
+  return <span className=" material-icons">edit</span>;
+};
