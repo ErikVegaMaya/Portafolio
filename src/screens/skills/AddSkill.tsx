b4fbd6a9cdbf75a7008ec4f3a42a14da4a3b0ca5
Erik Vega <erikvm95@gmail.com>
2023-01-18 19:06:17 -0600

    save skill feature added

--- src/screens/skills/AddSkill.tsx
+++ src/screens/skills/AddSkill.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { CloseIcon } from "../../components/Icons";
+// Hooks
+import useSaveSkill from "../../hooks/skills/useSaveSkill";
 
 const AddSkill = (props: any) => {
   const initialValues = {
@@ -8,6 +10,34 @@ const AddSkill = (props: any) => {
     level: "",
   };
   const [form, setForm] = React.useState(initialValues);
+  const [typeOfSkill, setTypeOfSkill] = React.useState("0");
+
+  const addSkillQuery = useSaveSkill();
+
+  const onSubmit = () => {
+    console.log(form);
+    const data = {
+      NameSkill: form.name,
+      TypeSkill: form.type,
+      LevelSkill: form.level,
+    };
+    console.log(data);
+    addSkillQuery.mutate(data, {
+      onError: async (error: any) => {
+        console.log("Error");
+        const requestErrors = error.response.data;
+        console.log(requestErrors);
+      },
+    });
+    props.onClick();
+  };
+
+  const catchChangeRadio = (value: string) => {
+    setTypeOfSkill(value);
+    console.log(value);
+    setForm({ ...form, type: value.toString(), level: "" });
+  };
+
   return (
     <div className="fixed inset-0 bg-gray-900 bg-opacity-60 transition-opacity">
       <div className="flex flex-col h-screen justify-center items-center">
@@ -52,7 +82,7 @@ const AddSkill = (props: any) => {
                 <div className="flex flex-row justify-between px-8">
                   <div className="flex flex-row">
                     <input
-                      onClick={() => setForm({ ...form, type: "1" })}
+                      onClick={() => catchChangeRadio("1")}
                       type="radio"
                       name="skill-type"
                       id="skill-programing"
@@ -63,7 +93,7 @@ const AddSkill = (props: any) => {
                   </div>
                   <div className="flex flex-row">
                     <input
-                      onClick={() => setForm({ ...form, type: "2" })}
+                      onClick={() => catchChangeRadio("2")}
                       type="radio"
                       name="skill-type"
                       id="skill-other"
@@ -81,17 +111,33 @@ const AddSkill = (props: any) => {
                 >
                   Level
                 </label>
-                <input
-                  value={form.level}
-                  onChange={(event) =>
-                    setForm({ ...form, level: event.target.value })
-                  }
-                  type="number"
-                  name="skill-level"
-                  placeholder="Enter"
-                  id="skill-level"
-                  className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
-                />
+                {typeOfSkill === "2" ? (
+                  <input
+                    value={form.level}
+                    onChange={(event) =>
+                      setForm({ ...form, level: event.target.value })
+                    }
+                    type="text"
+                    name="skill-level"
+                    placeholder="Enter"
+                    id="skill-level"
+                    className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                  />
+                ) : (
+                  <input
+                    value={form.level}
+                    onChange={(event) =>
+                      setForm({ ...form, level: event.target.value })
+                    }
+                    type="number"
+                    min={0}
+                    max={100}
+                    name="skill-level"
+                    placeholder="Enter"
+                    id="skill-level"
+                    className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                  />
+                )}
               </div>
             </div>
             <div className="mt-6 flex flex-row items-center justify-center ">
@@ -102,7 +148,7 @@ const AddSkill = (props: any) => {
                 Cancel
               </button>
               <button
-                onClick={() => {}}
+                onClick={onSubmit}
                 className="border rounded-lg bg-green-700 hover:bg-gray-900 px-4 py-1 ml-1 text-white"
               >
                 Save
--- src/services/request-api.ts
+++ src/services/request-api.ts
@@ -5,6 +5,12 @@ class AppRequest {
         console.log(api_url + url);
         return axios.get(api_url + url);
     };
+
+    post = async (url: string, body = {}) => {
+        console.log(api_url + url);
+        console.log(body);
+        return axios.post(api_url + url, body);
+    }
 }
 
 const api_url = "https://localhost:7233/api";
